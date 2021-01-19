48a8f918dd47a65e60b865509a62e26160b21869
Hyesu0220 <nrk2023@gmail.com>
2021-01-19 23:57:55 +0900

    가변 길이 데이터 송수신 처리 추가

---
+++ 02_SingleServer_variable/Client/Client/TCPClient.cpp
@@ -0,0 +1,141 @@
+#pragma comment(lib, "ws2_32")
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define SERVERIP "127.0.0.1"
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+// @see	https://docs.microsoft.com/en-us/windows/win32/api/winsock2/
+// @see https://docs.microsoft.com/en-us/windows/win32/winsock/complete-client-code
+
+/**
+ * 소켓 함수 오류를 Message Box로 출력 후 프로그램을 종료시키는 함수
+ *
+ * @param msg	소켓 함수 오류와 함께 출력할 메시지 (Caption)
+ */
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)& lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+/**
+ * 소켓 함수 오류를 console에 출력시키는 함수
+ *
+  * @param msg	소켓 함수 오류와 함께 출력할 메시지 (대활호 안의 내용)
+ */
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)& lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int RecvFixedSize(SOCKET in_socket, char* inout_buffer, int in_fixedsize, int flags)
+{
+	int recievedSize = 0;
+	char* currentBufPosition = inout_buffer;
+	int leftSize = in_fixedsize;
+
+	while (leftSize > 0)
+	{
+		recievedSize = recv(in_socket, currentBufPosition, leftSize, flags);
+		if (recievedSize == SOCKET_ERROR)
+		{
+			return SOCKET_ERROR;
+		}
+		else if (recievedSize == 0)
+		{
+			// 접속 종료됨을 의미
+			break;
+		}
+
+		leftSize -= recievedSize;
+		currentBufPosition += recievedSize;
+	}
+	return (in_fixedsize - leftSize);
+}
+
+int main(int argc, char* argv[])
+{
+	int iResult = 0;
+
+	// 윈속 초기화
+	WSADATA wsaData;
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+		err_quit("WSAStartup failed");
+
+	// socket()
+	SOCKET connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (connectSocket == INVALID_SOCKET)
+		err_quit("socket failed");
+
+	// 주소 구조체 준비
+	SOCKADDR_IN serverAddr;
+	ZeroMemory(&serverAddr, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	serverAddr.sin_port = htons(SERVERPORT);
+	
+	// connect()
+	iResult = connect(connectSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (iResult == SOCKET_ERROR)
+		err_quit("connect failed");
+
+	// 데이터 통신을 위한 변수 준비
+	char buf[BUFSIZE + 1];
+	int length = 0;
+
+	printf("INVALID_SOCKET VALUE : %d\n", INVALID_SOCKET);
+
+	while (true)
+	{
+		// 데이터 입력 받기
+		printf("\n[보낼 데이터] ");
+		if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
+			break;
+
+		// '\n' 제거
+		length = strlen(buf);
+		if (buf[length - 1] == '\n')
+			buf[length - 1] = '\0';
+		if (strlen(buf) == 0)
+			break;
+
+		// 데이터 크기 보내기
+		iResult = send(connectSocket, (char*)&length, sizeof(int), 0);
+		if (iResult == SOCKET_ERROR)
+		{
+			err_display("send failed.");
+			closesocket(connectSocket);
+			break;
+		}
+
+		printf("[TCP 클라이언트] 데이터 크기를 보냈습니다. (%d Bytes)\n", length);		
+
+		// 데이터 보내기
+		iResult = send(connectSocket, buf, length, 0);
+		if (iResult == SOCKET_ERROR)
+		{
+			err_display("send failed.");
+			closesocket(connectSocket);
+			break;
+		}
+		printf("[TCP 클라이언트] 데이터 %d Bytes를 보냈습니다.\n", iResult);
+		memset(&buf, 0, sizeof(buf));
+	}
+
+	if (connectSocket != INVALID_SOCKET)
+		closesocket(connectSocket);
+
+	WSACleanup();
+}
--- README.md
+++ README.md
@@ -8,9 +8,12 @@
 
 
 ## 목차
-[1. 싱글 스레드 / 1대1 TCP 통신](#1-싱글-스레드--1대1-tcp-통신)
 
+[1. 싱글 스레드 / 1대1 TCP 통신 ](#1-싱글-스레드--1대1-tcp-통신)
 
+[2. 싱글 스레드 / 가변 길이 데이터 통신](#2-싱글-스레드--가변-길이-데이터-통신)
+
+----
 ### 1. 싱글 스레드 / 1대1 TCP 통신
 #### 서버
 @see [MSDN 서버 예시 코드](https://docs.microsoft.com/en-us/windows/win32/winsock/complete-server-code)
@@ -39,3 +42,28 @@
 > recv(), send()
 6. 연결이 종료된 클라이언트 소켓은 closesocket()으로 완전히 닫아준다.
 7. WSACleanup()으로 WSADATA를 정리한다.
+
+----
+### 2. 싱글 스레드 / 가변 길이 데이터 통신
+#### 서버 통신 과정
+1. WSADATA 초기화
+2. listen socket 생성
+3. 서버의 주소 정보가 담긴 SOCKADDR 준비
+4. listen socket 과 SOCKADDR 바인드
+5. 바인딩이 완료된 listen socket을 listening 상태로 만든다.
+6. accept()로 클라이언트의 connect()를 받는다.
+7. int(4byte)를 받는다. ( 데이터 크기 수신 )
+8. 7에서 받은 크기 만큼 받는다. ( 데이터 수신 )
+9. 연결이 종료된 클라이언트 소켓은 closesocket()으로 완전히 닫아준다.
+10. WSACleanup()으로 WSADATA를 정리한다.
+
+#### 클라이언트 통신 과정
+1. WSADATA 초기화
+2. 서버와 통신용 socket 생성
+3. 서버의 주소 정보가 담긴 SOCKADDR 준비
+4. 서버의 socket과 SOCKADDR로 connect() 요청한다.
+5. 사용자에게 보낼 데이터를 입력받는다.
+6. int(4byte)를 보낸다. ( 데이터 크기 송신 )
+7. 6에서 보낸 int 값 만큼 만큼 보낸다. ( 데이터 송신 )
+8. 연결이 종료된 클라이언트 소켓은 closesocket()으로 완전히 닫아준다.
+9. WSACleanup()으로 WSADATA를 정리한다.
